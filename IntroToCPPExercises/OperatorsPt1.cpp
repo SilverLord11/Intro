a68f4f331f9febf921a562b1d0c18e234cfcdf4e
Logan Holt <s209059@students.aie.edu>
2020-11-19 13:01:36 -0800

    Operator

---
+++ IntroToCPPExercises/OperatorsPt1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+void operators() 
+{
+	int first = 0;
+
+	std::cout << "Enter the first number." << std::endl;
+
+	std::cin >> first;
+	if (std::cin.fail())
+	{
+		std::cout << "You didn't enter an integer.";
+		std::cout << "So, we will just use '5' instead." << std::endl;
+		std::cin.clear();
+		std::cin.ignore(std::cin.rdbuf()->in_avail());
+	}
+}
