d35cc6d81f8c0ed4c49dfed9b957a0d94993693b
Logan Holt <s209059@students.aie.edu>
2020-11-19 16:34:07 -0800

    So had to regrab some stuff

--- IntroToCPPExercises/OperatorsPt1.cpp
+++ IntroToCPPExercises/OperatorsPt1.cpp
@@ -3,10 +3,12 @@
 void main() 
 {
 	int first = 0;
+	int second = 0;
 
 	std::cout << "Enter the first number." << std::endl;
-
 	std::cin >> first;
+
+
 	if (std::cin.fail())
 	{
 		std::cout << "You didn't enter an integer.";
@@ -14,4 +16,25 @@ void main()
 		std::cin.clear();
 		std::cin.ignore(std::cin.rdbuf()->in_avail());
 	}
+
+	std::cout << "Enter the second number: " << std::endl;  
+	std::cin >> second;
+
+	if (std::cin.fail()) 
+	{ 
+		std::cout << "You didn't enter an integer. We'll just use '8' instead." << std::endl;   
+		second = 8;   
+		std::cin.clear();   
+		std::cin.ignore(std::cin.rdbuf()->in_avail()); 
+	}
+
+	std::cout << "The two numbers entered are: " << first << " and " << second << "." << std::endl;  
+	std::cout << first << " + " << second << " = " << (first + second) << std::endl;  
+	std::cout << first << " - " << second << " = " << (first - second) << std::endl;  
+	std::cout << first << " / " << second << " = " << (first / second) << std::endl;  
+	std::cout << first << " * " << second << " = " << (first * second) << std::endl;  
+	std::cout << first << " % " << second << " = " << (first % second) << std::endl;
+
+	std::cin.ignore(std::cin.rdbuf()->in_avail());  
+	std::cin.get();
 }
